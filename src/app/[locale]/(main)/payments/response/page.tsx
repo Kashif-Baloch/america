78129f355f1ab6b1483eb434744189994c1f22e5
Kashif <kashifkhan451810@gmail.com>
2025-08-16 15:09:29 +0500

    feat: added page redirect

--- src/app/[locale]/(main)/payments/response/page.tsx
+++ src/app/[locale]/(main)/payments/response/page.tsx
@@ -1,96 +1,203 @@
+// "use client";
+// import { Loader2Icon } from "lucide-react";
+// import { useEffect, useState } from "react";
+
+// export default function PaymentResponsePage() {
+//   const [paymentData, setPaymentData] = useState<null | {
+//     status: string;
+//     referenceCode: string;
+//     message: string;
+//     description: string;
+//     buyerEmail: string;
+//     paymentMethod: string;
+//   }>(null);
+
+//   useEffect(() => {
+//     const params = new URLSearchParams(window.location.search);
+//     const referenceCode = params.get("referenceCode");
+//     const status = params.get("lapTransactionState");
+//     const message = params.get("message");
+//     const description = params.get("description");
+//     const buyerEmail = params.get("buyerEmail");
+//     const paymentMethod = params.get("lapPaymentMethod");
+
+//     if (referenceCode) {
+//       fetch(
+//         `/api/payments/confirmation?referenceCode=${referenceCode}&status=${status}`
+//       )
+//         .then((res) => res.json())
+//         .then((data) => {
+//           setPaymentData({
+//             status: data.status,
+//             referenceCode: referenceCode || "",
+//             message: message || "",
+//             description: description || "",
+//             buyerEmail: buyerEmail || "",
+//             paymentMethod: paymentMethod || "",
+//           });
+//         });
+//     }
+//   }, []);
+
+//   if (!paymentData)
+//     return (
+//       <div className="flex items-center justify-center h-[100vh]">
+//         <p className="text-xl font-bold flex items-center justify-center gap-3">
+//           <Loader2Icon size={40} className="animate-spin" />
+//           Loading payment status...
+//         </p>
+//       </div>
+//     );
+
+//   return (
+//     <div className="flex items-center justify-center h-[100vh] ">
+//       <div className="bg-gray-50 p-6 rounded-lg w-[500px]">
+//         <h2 className="text-2xl font-bold mb-4">Payment Details</h2>
+//         {paymentData.status === "APPROVED" ? (
+//           <h2 className="text-green-600 font-bold text-lg">
+//             ✅ Payment Successful!
+//           </h2>
+//         ) : paymentData.status === "DECLINED" ? (
+//           <h2 className="text-red-600 font-bold text-lg">
+//             ❌ Payment Declined
+//           </h2>
+//         ) : (
+//           <h2 className="text-yellow-600 font-bold text-lg">
+//             ⏳ Payment Pending...
+//           </h2>
+//         )}
+//         <p className="my-4">
+//           <span className="text-lg font-bold text-gray-600">
+//             Reference Code
+//           </span>{" "}
+//           : {paymentData.referenceCode}
+//         </p>
+//         <p className="my-4">
+//           <span className="text-lg font-bold text-gray-600">Message</span> :{" "}
+//           {paymentData.message}
+//         </p>
+//         <p className="my-4">
+//           <span className="text-lg font-bold text-gray-600">Description</span> :{" "}
+//           {paymentData.description}
+//         </p>
+//         <p className="my-4">
+//           <span className="text-lg font-bold text-gray-600">Buyer Email</span> :{" "}
+//           {paymentData.buyerEmail}
+//         </p>
+//         <p className="my-4">
+//           <span className="text-lg font-bold text-gray-600">
+//             Payment Method
+//           </span>{" "}
+//           : {paymentData.paymentMethod}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
+
 "use client";
-import { Loader2Icon } from "lucide-react";
+
+import { Check, Loader2Icon } from "lucide-react";
+import Image from "next/image";
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function PaymentResponsePage() {
-  const [paymentData, setPaymentData] = useState<null | {
-    status: string;
-    referenceCode: string;
-    message: string;
-    description: string;
-    buyerEmail: string;
-    paymentMethod: string;
-  }>(null);
+export default function ThankYouPage() {
+  const t = useTranslations("thankyou");
+  const [showPage, setShowPage] = useState(true);
+  const router = useRouter();
+
+  const benefits = t.raw("benefits") as string[];
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const referenceCode = params.get("referenceCode");
     const status = params.get("lapTransactionState");
-    const message = params.get("message");
-    const description = params.get("description");
-    const buyerEmail = params.get("buyerEmail");
-    const paymentMethod = params.get("lapPaymentMethod");
 
-    if (referenceCode) {
+    if (!referenceCode) {
+      router.push("/pricing");
+    } else {
       fetch(
         `/api/payments/confirmation?referenceCode=${referenceCode}&status=${status}`
       )
         .then((res) => res.json())
-        .then((data) => {
-          setPaymentData({
-            status: data.status,
-            referenceCode: referenceCode || "",
-            message: message || "",
-            description: description || "",
-            buyerEmail: buyerEmail || "",
-            paymentMethod: paymentMethod || "",
-          });
+        .then(() => {
+          setShowPage(false);
         });
     }
   }, []);
 
-  if (!paymentData)
+  if (showPage) {
     return (
-      <div className="flex items-center justify-center h-[100vh]">
-        <p className="text-xl font-bold flex items-center justify-center gap-3">
-          <Loader2Icon size={40} className="animate-spin" />
-          Loading payment status...
-        </p>
+      <div className="h-[100vh] flex items-center justify-center">
+        <Loader2Icon size={40} className="animate-spin" />
       </div>
     );
+  }
 
   return (
-    <div className="flex items-center justify-center h-[100vh] ">
-      <div className="bg-gray-50 p-6 rounded-lg w-[500px]">
-        <h2 className="text-2xl font-bold mb-4">Payment Details</h2>
-        {paymentData.status === "APPROVED" ? (
-          <h2 className="text-green-600 font-bold text-lg">
-            ✅ Payment Successful!
+    <div className="py-12 px-4 sm:px-6 font-sf lg:px-8">
+      <div className="max-w-3xl mx-auto overflow-hidden p-8">
+        <div className="flex justify-center mb-10">
+          <Image
+            height={900}
+            width={900}
+            src="/images/logo.webp"
+            alt="America Working Logo"
+            className="h-24 w-auto"
+          />
+        </div>
+
+        <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-900 mb-4">
+          {t("title")}
+        </h1>
+
+        <p className="text-lg text-center text-gray-600 mb-8 whitespace-pre-line">
+          {t("caption")}
+          <span className="fi fi-us ml-1 text-xl"></span>
+          <br />
+          <br />
+
+          {t("caption2")}
+        </p>
+
+        <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-4 mb-12">
+          <Link
+            href="/"
+            className="hidden md:inline-flex justify-center items-center bg-primary-blue cursor-pointer text-white w-60 md:h-14 h-12 text-lg py-2 rounded-full font-semibold hover:bg-white hover:text-primary-blue border border-primary-blue duration-300"
+          >
+            {t("button_job_search")}
+          </Link>
+          <Link
+            href="/settings"
+            className="hidden md:inline-flex justify-center items-center hover:bg-primary-blue cursor-pointer hover:text-white w-44 md:h-14 h-12 text-lg py-2 rounded-full font-semibold bg-white text-primary-blue border border-primary-blue duration-300"
+          >
+            {t("button_user_panel")}
+          </Link>
+        </div>
+
+        <div className="bg-gray-50 rounded-2xl p-6">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+            {t("benefits_title")}
           </h2>
-        ) : paymentData.status === "DECLINED" ? (
-          <h2 className="text-red-600 font-bold text-lg">
-            ❌ Payment Declined
-          </h2>
-        ) : (
-          <h2 className="text-yellow-600 font-bold text-lg">
-            ⏳ Payment Pending...
-          </h2>
-        )}
-        <p className="my-4">
-          <span className="text-lg font-bold text-gray-600">
-            Reference Code
-          </span>{" "}
-          : {paymentData.referenceCode}
-        </p>
-        <p className="my-4">
-          <span className="text-lg font-bold text-gray-600">Message</span> :{" "}
-          {paymentData.message}
-        </p>
-        <p className="my-4">
-          <span className="text-lg font-bold text-gray-600">Description</span> :{" "}
-          {paymentData.description}
-        </p>
-        <p className="my-4">
-          <span className="text-lg font-bold text-gray-600">Buyer Email</span> :{" "}
-          {paymentData.buyerEmail}
-        </p>
-        <p className="my-4">
-          <span className="text-lg font-bold text-gray-600">
-            Payment Method
-          </span>{" "}
-          : {paymentData.paymentMethod}
-        </p>
+          <ul className="space-y-4">
+            {benefits.map((text, index) => (
+              <BenefitItem key={index} text={text} />
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
+  );
+}
+
+function BenefitItem({ text }: { text: string }) {
+  return (
+    <li className="flex items-start">
+      <Check className="h-5 w-5 text-green-500 mt-0.5 mr-3 flex-shrink-0" />
+      <span className="text-gray-700 text-lg">{text}</span>
+    </li>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -11,49 +11,56 @@ const intlMiddleware = createMiddleware(routing);
 // type Session = typeof auth.$Infer.Session;
 
 export default async function middleware(request: NextRequest) {
-    const { pathname, origin, search } = request.nextUrl;
+  const { pathname, origin, search } = request.nextUrl;
 
-    // Extract locale prefix (e.g. "/en", "/es")
-    const localeMatch = pathname.match(/^\/(en|es|pt)(?=\/|$)/);
-    const localePrefix = localeMatch?.[0] || '';
-    const pathWithoutLocale = pathname.replace(/^\/(en|es|pt)(?=\/|$)/, '');
+  // Extract locale prefix (e.g. "/en", "/es")
+  const localeMatch = pathname.match(/^\/(en|es|pt)(?=\/|$)/);
+  const localePrefix = localeMatch?.[0] || "";
+  const pathWithoutLocale = pathname.replace(/^\/(en|es|pt)(?=\/|$)/, "");
 
-    console.log('Accessing route:', pathWithoutLocale);
+  console.log("Accessing route:", pathWithoutLocale);
 
-    const isProtected = pathWithoutLocale.startsWith("/admin") || pathWithoutLocale.startsWith("/settings");
-    const isAuthPage = pathWithoutLocale === "/login" || pathWithoutLocale === "/sign-in";
+  const isProtected =
+    pathWithoutLocale.startsWith("/admin") ||
+    pathWithoutLocale.startsWith("/settings");
+  const isAuthPage =
+    pathWithoutLocale === "/login" || pathWithoutLocale === "/sign-in";
 
-    if (isProtected || isAuthPage) {
-        // for checking whole session 
+  if (isProtected || isAuthPage) {
+    // for checking whole session
 
-        // const { data: session } = await $fetch<Session>("/api/auth/get-session", {
-        //     baseURL: origin,
-        //     headers: {
-        //         cookie: request.headers.get("cookie") || "",
-        //     },
-        // });
+    // const { data: session } = await $fetch<Session>("/api/auth/get-session", {
+    //     baseURL: origin,
+    //     headers: {
+    //         cookie: request.headers.get("cookie") || "",
+    //     },
+    // });
 
-        // for checking only session existance ( make sure to have page level auth check also if using this  ) 
+    // for checking only session existance ( make sure to have page level auth check also if using this  )
 
-        const session = getSessionCookie(request)
+    const session = getSessionCookie(request);
 
-        console.log("Session in middleware :- ", { session })
+    console.log("Session in middleware :- ", { session });
 
-        if (isProtected && !session) {
-            return NextResponse.redirect(new URL(`${localePrefix}/login${search}`, origin));
-        }
-
-        if (isAuthPage && session) {
-            return NextResponse.redirect(new URL(`${localePrefix}/settings${search}`, origin));
-        }
+    if (isProtected && !session) {
+      return NextResponse.redirect(
+        new URL(`${localePrefix}/login${search}`, origin)
+      );
     }
 
-    return intlMiddleware(request);
+    if (isAuthPage && session) {
+      return NextResponse.redirect(
+        new URL(`${localePrefix}/settings${search}`, origin)
+      );
+    }
+  }
+
+  return intlMiddleware(request);
 }
 
 export const config = {
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
